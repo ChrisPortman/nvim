115936b22709361502c44a192fe777136240ab88
Chris Portman <chris@portman.net.au>
2024-02-05 14:27:55 +1100

    prompt when attempting to close modified buffer

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,11 +6,18 @@ return {
 		require("bufferline").setup({
 			options = {
 				close_command = function(buf)
-					local ok, result = pcall(vim.cmd, "bdelete " .. buf)
-					if ok then
-						return
+					if vim.api.nvim_buf_get_option(buf, "modified") then
+						print("buffer is modified - need user check to delete without saving")
+						vim.ui.input({
+							prompt = "Buffer has unsaved changes, close without saving? [y/N]: ",
+						}, function(i)
+							if i:lower() == "y" then
+								vim.api.nvim_buf_delete(buf, { force = true })
+							end
+						end)
+					else
+						vim.api.nvim_buf_delete(buf, {})
 					end
-					print(result)
 				end,
 			},
 		})
